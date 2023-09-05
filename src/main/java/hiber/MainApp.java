98353b20520caccb31b690954471e1618e6d04e3
defeatedSanity <rome.kuznets@gmail.com>
2023-09-05 12:04:08 +0300

    deleted comments

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -37,22 +37,6 @@ public class MainApp {
 
       System.out.println(userService.getUserByCar("Jetta", 2));
 
-
-
-//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-//
-//      List<User> users = userService.listUsers();
-//      for (User user : users) {
-//         System.out.println("Id = "+user.getId());
-//         System.out.println("First Name = "+user.getFirstName());
-//         System.out.println("Last Name = "+user.getLastName());
-//         System.out.println("Email = "+user.getEmail());
-//         System.out.println();
-//      }
-
       context.close();
    }
 }
